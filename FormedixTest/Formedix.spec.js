bfade3d4d88600b47dafd80b5ccaf24c3652d339
gabriellynel <lyneladgabriel@gmail.com>
2023-08-15 10:51:43 +0800

    1st Commit

---
+++ FormedixTest/Formedix.spec.js
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+import { Formedix } from '../FormedixPOM/FormedixPOM'
+
+test('Formedix', async ({ page }) => {
+
+    const FormedixTest = new Formedix(page)
+
+    await FormedixTest.NavigateToLoginPage()
+    await FormedixTest.SignIn('testteamtechtest', 'Ryz3T3st!x1')
+    await FormedixTest.NavigateToForms()
+    await FormedixTest.UpdateMedicalHistory('DescriptionUpdate1', 'LocaleUpdate1', 'DescriptionUpdate1')
+
+});
